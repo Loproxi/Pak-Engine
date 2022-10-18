962f6017ad574b7b5d0c676d0af420e81039b253
Loproxi <79161178+Loproxi@users.noreply.github.com>
2022-10-18 21:52:52 +0200

    Project properties Update

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -276,9 +276,11 @@ bool ModuleRenderer3D::Init(pugi::xml_node& config)
 //	document.Accept(writer);    // Accept() traverses the DOM and generates Handler events.
 //	puts(sb.GetString());
 
-	//cube = new Cube(4.0f, 4.0f, 4.0f);
+	cube = new Cube(4.0f, 4.0f, 4.0f);
 
-	//test = new Mesh(cube->GetVertices(), cube->GetNumVertices(), cube->Getindices(), cube->GetNumIndices(), float3{ 2.0f,1.0f,4.0f }, float3{ 0.0f,0.0f,0.0f }, float3{ 2.0f,2.0f,2.0f });
+	test = new Mesh(cube->GetVertices(), cube->GetNumVertices(), cube->Getindices(), cube->GetNumIndices(), float3{ 2.0f,1.0f,4.0f }, float3{ 0.0f,0.0f,0.0f }, float3{ 2.0f,2.0f,2.0f });
+	//test = new Mesh(cube.GetVertices(), cube.GetNumVertices(), cube.Getindices(), cube.GetNumIndices(), float3{ 2.0f,1.0f,4.0f }, float3{ 0.0f,0.0f,0.0f }, float3{ 2.0f,2.0f,2.0f });
+
 	
 	house.Import("../Output/Assets/BakerHouse.fbx");
 
@@ -347,7 +349,7 @@ UpdateStatus ModuleRenderer3D::PostUpdate()
 		EndDebugDraw();
 	}*/
 	
-	//test->RenderMeshes();
+	test->RenderMeshes();
 
 	//Mesh test2(sphere.GetVertices(), sphere.GetNumVertices(), sphere.Getindices(), sphere.GetNumIndices());
 
